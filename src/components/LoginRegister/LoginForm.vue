9e659748f21e766245cd13c6694497478cfa0efd
Mensu <yxshw55@qq.com>
2018-05-30 10:29:18 +0800

    feature: 输入框密码可以显示

--- src/components/LoginRegister/LoginForm.vue
+++ src/components/LoginRegister/LoginForm.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <mu-text-field class="w-p100" label="用户名" icon="person" labelFloat
-                    v-model="username" @keyup.native.enter="doLogin()"/>
+                    v-model="username" @keyup.enter="doLogin()"/>
     <mu-text-field class="w-p100" label="密码" icon="lock" labelFloat
-                   type="password" v-model="password" @keyup.native.enter="doLogin()"/>
+                   v-model="password"
+                   :type="passwordIsVisible ? 'text' : 'password'"
+                   :action-icon="passwordIsVisible ? 'visibility_off' : 'visibility'"
+                   :action-click="() => passwordIsVisible = !passwordIsVisible"
+                   @keyup.enter="doLogin()"/>
     <mu-button class="w-p100 mt-30" @click="doLogin()" :disabled="isLoggingIn" color="primary">
       {{ isLoggingIn ? '登录中...' : '登录' }}
     </mu-button>
@@ -22,6 +26,7 @@ export default class extends Vue {
   username = '';
   password = '';
   isLoggingIn = false;
+  passwordIsVisible = false;
   @Action(LOGIN) login;
 
   reset() {
