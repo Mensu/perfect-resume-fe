dd7a6356771c97024374ad16c9b07d75dbdc66ce
Mensu <yxshw55@qq.com>
2018-05-30 23:30:01 +0800

    store: 使用对接了的 API

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,6 +3,8 @@ export const OPEN_LOGIN_REGISTER = 'OPEN_LOGIN_REGISTER';
 export const CLOSE_LOGIN_REGISTER = 'CLOSE_LOGIN_REGISTER';
 export const SET_LOGIN_FORM_CLOSABLE = 'SET_LOGIN_FORM_CLOSABLE';
 export const SET_ADMIN = 'SET_ADMIN';
+export const SET_SNAKEBAR_MSG = 'SET_SNAKEBAR_MSG';
+export const INCREMENT_SNAKEBAR_MSG_ID = 'INCREMENT_SNAKEBAR_MSG_ID';
 
 export const mutations = {
   [SET_LOGIN_STATE](state, isLoggedIn) {
@@ -22,5 +24,11 @@ export const mutations = {
   },
   [SET_ADMIN](state, isAdmin) {
     state.isAdmin = isAdmin;
+  },
+  [SET_SNAKEBAR_MSG](state, msg) {
+    state.snakebarMsg = msg;
+  },
+  [INCREMENT_SNAKEBAR_MSG_ID](state) {
+    state.snakebarMsgId += 1;
   },
 };
