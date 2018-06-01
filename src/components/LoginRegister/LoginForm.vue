d98fbecaf8b827c183036cf957b41aee462f8f3f
Mensu <yxshw55@qq.com>
2018-06-01 18:19:33 +0800

    feature: 用户名必须为int

--- src/components/LoginRegister/LoginForm.vue
+++ src/components/LoginRegister/LoginForm.vue
@@ -1,7 +1,9 @@
 <template>
   <form>
     <mu-text-field class="w-p100" label="用户名" icon="person" label-float autocomplete="username"
-                    v-model="username" @keyup.enter="doLogin()"/>
+                    v-model="username"
+                    :error-text="error && error.username"
+                    @keyup.enter="doLogin()"/>
     <mu-text-field class="w-p100" label="密码" icon="lock" label-float autocomplete="current-password"
                    v-model="password"
                    :type="passwordIsVisible | visibilityInputType"
@@ -9,7 +11,7 @@
                    :action-click="() => passwordIsVisible = !passwordIsVisible"
                    @keyup.enter="doLogin()"/>
     <mu-button class="w-p100 mt-30" color="primary" @click="doLogin()"
-              :disabled="username && password && isLoggingIn">
+              :disabled="!username || !password || !isValid || isLoggingIn">
       {{ isLoggingIn ? '登录中...' : '登录' }}
     </mu-button>
   </form>
@@ -32,12 +34,28 @@ export default class extends Vue {
   isLoggingIn = false;
   passwordIsVisible = false;
 
+  get error() {
+    const error = {};
+    if (this.username.length > 0) {
+      if (!this.username.match(/^[1-9][0-9]{0,}$/)) {
+        error.username = '用户名只能含数字';
+      } else if (Number(this.username) >= 2 ** 31) {
+        error.username = '用户名必须为 int';
+      }
+    }
+    return error;
+  }
+
+  get isValid() {
+    return Object.keys(this.error).length === 0;
+  }
+
   async doLogin() {
     if (this.isLoggingIn) {
       return;
     }
     const { username, password } = this;
-    if (username.length === 0 || password.length === 0) {
+    if (!this.isValid || username.length === 0 || password.length === 0) {
       return;
     }
     this.isLoggingIn = true;
